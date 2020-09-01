ac84bd7e380d415c910320d60f067a590b134fe8
Danilo Lutz <danilolutz@protonmail.com>
2020-09-01 16:09:35 -0300

    Deploy 2

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -12,7 +12,7 @@ describe('ListProviderMonthAvailability', () => {
     );
   });
 
-  it('should be able to list list the month availability of a provider', async () => {
+  it('should be able to list the month availability of a provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
       user_id: 'user',
@@ -25,60 +25,6 @@ describe('ListProviderMonthAvailability', () => {
       date: new Date(2020, 4, 20, 9, 0, 0),
     });
 
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      user_id: 'user',
-      date: new Date(2020, 4, 20, 10, 0, 0),
-    });
-
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      user_id: 'user',
-      date: new Date(2020, 4, 20, 11, 0, 0),
-    });
-
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      user_id: 'user',
-      date: new Date(2020, 4, 20, 12, 0, 0),
-    });
-
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      user_id: 'user',
-      date: new Date(2020, 4, 20, 13, 0, 0),
-    });
-
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      user_id: 'user',
-      date: new Date(2020, 4, 20, 14, 0, 0),
-    });
-
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      user_id: 'user',
-      date: new Date(2020, 4, 20, 15, 0, 0),
-    });
-
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      user_id: 'user',
-      date: new Date(2020, 4, 20, 16, 0, 0),
-    });
-
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      user_id: 'user',
-      date: new Date(2020, 4, 20, 17, 0, 0),
-    });
-
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      user_id: 'user',
-      date: new Date(2020, 4, 21, 17, 0, 0),
-    });
-
     const availability = await listProviderMonthAvailability.execute({
       provider_id: 'user',
       year: 2020,
@@ -88,7 +34,7 @@ describe('ListProviderMonthAvailability', () => {
     expect(availability).toEqual(
       expect.arrayContaining([
         { day: 20, available: false },
-        { day: 21, available: true },
+        { day: 21, available: false },
       ]),
     );
   });
